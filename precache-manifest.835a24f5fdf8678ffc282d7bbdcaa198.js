0ea81716a67b308efbc91205605055c65052a681
apurvgupta-ops <56751664+apurvgupta-ops@users.noreply.github.com>
2020-10-22 21:35:00 +0530

    Updates

---
+++ precache-manifest.835a24f5fdf8678ffc282d7bbdcaa198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce1612316f6335f39bb6bba56d72354",
+    "url": "/React-secret-note/index.html"
+  },
+  {
+    "revision": "fadd5919768c9c7079cf",
+    "url": "/React-secret-note/static/css/main.00cc585a.chunk.css"
+  },
+  {
+    "revision": "7e085900174779f1b53c",
+    "url": "/React-secret-note/static/js/2.0ec8d584.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-secret-note/static/js/2.0ec8d584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fadd5919768c9c7079cf",
+    "url": "/React-secret-note/static/js/main.400e0c63.chunk.js"
+  },
+  {
+    "revision": "c515d6ad1fb7809f15ff",
+    "url": "/React-secret-note/static/js/runtime-main.0cb38d91.js"
+  }
+]);
